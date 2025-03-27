3badcd05daedeac729d80f97d976269ad5c912c3
Achut Hegde <achuthegde@Achuts-MacBook-Air.local>
2025-03-27 18:36:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonalBranding from './components/PersonalBranding';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalBranding />
+    </div>
+  );
+}
+
+export default App; 
